283c2b71907cc6957bd584b187fd1f091d66d6dc
Noelsip <noelsipayung45@gmail.com>
2025-07-03 15:43:10 +0800

    update konten 1

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Siapa Saya?",
   description1:
-    "Nama saya Noel Ericson Rapael Sipayung, mahasiswa Informatika di Institut Teknologi Kalimantan (ITK) yang memiliki fokus kuat dalam pengembangan backend, perancangan API, dan arsitektur sistem. Saya terbiasa membangun RESTful API yang skalabel, mengelola database cloud, serta mengintegrasikan layanan pihak ketiga seperti Google Drive, Payment Gateway, dan Marketplace API.",
+    "Nama saya Noel Ericson Rapael Sipayung, mahasiswa Informatika di Institut Teknologi Kalimantan (ITK) yang memiliki fokus  dalam pengembangan backend, perancangan API, dan arsitektur sistem, IoT, dan AI. Saya terbiasa membangun RESTful API yang skalabel, mengelola database cloud, serta mengintegrasikan layanan pihak ketiga seperti Google Drive, Payment Gateway, dan Marketplace API.",
   description2:
     "Dengan latar belakang yang kuat di Node.js, MySQL, dan Java (termasuk JavaFX), saya telah mengerjakan berbagai proyek nyata seperti platform pengumpulan tugas, sistem autentikasi, simulasi smart home, hingga mesin pencari berbasis struktur data Red-Black Tree. Saya juga memiliki ketertarikan pada pengembangan AI/ML serta sering bereksperimen dengan OpenCV dan integrasi IoT. Tujuan saya adalah membangun sistem backend yang andal, efisien, dan terintegrasi dengan baik.",
   image: 1
--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'noelsipayung45@gmail.com',
-    phone: '++6282165334792',
+    phone: '+6282165334792',
     address: 'Balikpapan, East Kalimantan, Indonesia',
 
     sheetAPI: ''
